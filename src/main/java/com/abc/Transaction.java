fdbe232b5376cc565a8285ebee876e76a055ffbb
lipak2345 <lipak2345@users.noreply.github.com>
2016-01-28 06:04:44 -0500

    Refactor Transactions to make it immutable.

--- src/main/java/com/abc/Transaction.java
+++ src/main/java/com/abc/Transaction.java
@@ -1,16 +1,50 @@
 package com.abc;
 
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.util.UUID;
 
-public class Transaction {
-    public final double amount;
+/**
+ * Immutable class to represent transaction.
+ */
+public final class Transaction {
 
-    private Date transactionDate;
-
-    public Transaction(double amount) {
-        this.amount = amount;
-        this.transactionDate = DateProvider.getInstance().now();
+    public UUID getTransactionId() {
+        return transactionId;
     }
 
+    private final UUID transactionId = UUID.randomUUID();
+
+    //TODO: Convert to a proper money/currency handling class or at least use BigDecimal for amount
+    private final double amount;
+
+    //Using Java 8 immutable LocalDateTime. for earlier versions of Java it is better to use something like Joda time
+    private final LocalDateTime transactionDate;
+
+    /**
+     * Ctor using current date-time for transaction.
+     *
+     * @param amount the amount of the transaction
+     */
+    public Transaction(double amount) {
+        this(amount, null);
+    }
+
+    /**
+     * Ctor which accepts the date and time of the transaction.
+     *
+     * @param amount the amount of the transaction
+     * @param date the date-time of the transaction. If null use the current date-time
+     */
+    public Transaction(double amount, LocalDateTime date) {
+        this.amount = amount;
+        this.transactionDate = (date != null)? date: LocalDateTime.now();
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public LocalDateTime getTransactionDate() {
+        return transactionDate;
+    }
 }
